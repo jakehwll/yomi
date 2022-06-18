6bb84823dd5f5fd61020eb738108ce78721062fb
Jake <jake@hwll.me>
2022-06-18 19:49:09 +1000

    feat: resolve renderer paging and readability

--- pages/book/[id]/read.tsx
+++ pages/book/[id]/read.tsx
@@ -1,6 +1,25 @@
+import cc from 'classcat'
+import { range } from 'lodash'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import styles from 'styles/Reader.module.scss'
+
+const Pages = ({ render }: { render: Array<string> }) => {
+  return (
+    <section>
+      <div className={styles.canvas}>
+        {render.map((v: string, i: number) => {
+          return (
+            <div className={styles.page} key={i}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={v} alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
 
 const Reader = () => {
   const router = useRouter()
@@ -8,27 +27,59 @@ const Reader = () => {
 
   // constants
   const [index, setIndex] = useState(0)
-  const [pageAmount, setPageAmount] = useState(1)
+  const [pageAmount, setPageAmount] = useState(2)
+  const [controls, setControls] = useState(true)
   const [render, setRender] = useState<Array<string>>([])
-  const [keyDown, setKeydown] = useState('')
 
   // controls
-  const [invert, setInvert] = useState(false)
+  const [invert, setInvert] = useState(true)
 
-  const _prev = () => {
-    setIndex(index - 1)
+  const _prev = (pageNum: number) => {
+    // dont allow to page out of bounds.
+    if (pageNum - 1 < 0) return 0
+    // add one to our page number to decrement.
+    return pageNum - 1
   }
-  const _next = () => {
-    setIndex(index + 1)
+  const _next = (pageNum: number) => {
+    // TODO dont allow to page out of bounds.
+    if (pageNum - -1 < 0) return -1
+    // add one to our page number to increment.
+    return pageNum + 1
   }
 
   useEffect(() => {
-    setRender([`/api/book/${id}/page/${index}`])
-  }, [index])
+    if (id === undefined) return setRender([])
+    // only render the front-cover.
+    if (index === 0) return setRender([`/api/book/${id}/page/0`])
+    // TODO only render the back-cover.
+    if (index === -1) return setRender([`/api/book/${id}/page/${-1}`])
+    // loop all pages for our pageAmount range and render them
+    let pages = range(index * pageAmount, index * pageAmount + pageAmount).map(
+      (v: number) => `/api/book/${id}/page/${v}`
+    )
+    // reverse if inverted layout (manga).
+    if (invert) pages = pages.reverse()
+    // update the renderer.
+    setRender(pages)
+  }, [id, index])
+
+  useEffect(() => {
+    const keyHandler = (event: KeyboardEvent) => {
+      console.log(event.key)
+      if (event.key === 'ArrowRight')
+        setIndex((pageNum) => (invert ? _prev(pageNum) : _next(pageNum)))
+      if (event.key === 'ArrowLeft')
+        setIndex((pageNum) => (invert ? _next(pageNum) : _prev(pageNum)))
+      if (event.key === ' ') setControls((controls) => !controls)
+      if (event.key === 'm') setControls((controls) => !controls)
+    }
+    document.addEventListener('keydown', keyHandler)
+    return () => document.removeEventListener('keydown', keyHandler)
+  }, [])
 
   return (
     <div className={styles.root}>
-      <header className={styles.header}>
+      <header className={cc([styles.header, { [styles.hidden]: !controls }])}>
         <div className={styles.back}>
           <button type="button" onClick={() => router.push(`/book/${id}`)}>
             &larr;
@@ -36,31 +87,23 @@ const Reader = () => {
         </div>
         <div className={styles.title}>Book Title</div>
         <div className={styles.tools}>
-          {/* <div>Fullscreen</div> */}
+          <button
+            onClick={() => {
+              !document.fullscreenElement
+                ? document.documentElement.requestFullscreen()
+                : document.exitFullscreen()
+            }}
+          >
+            Fullscreen
+          </button>
           {/* <div>Settings</div> */}
           {/* <div>Download</div> */}
         </div>
       </header>
-      <section>
-        <div className={styles.canvas}>
-          {render.map((v: string, i: number) => {
-            return (
-              <div className={styles.page} key={i}>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={v} alt="" />
-              </div>
-            )
-          })}
-        </div>
-      </section>
-      <footer className={styles.footer}>
-        {index}
-        <button type="button" onClick={_prev}>
-          -
-        </button>
-        <button type="button" onClick={_next}>
-          +
-        </button>
+      <Pages render={render} />
+      <footer className={cc([styles.footer, { [styles.hidden]: !controls }])}>
+        <div>{index}</div>
+        <div>{controls.toString()}</div>
       </footer>
     </div>
   )
