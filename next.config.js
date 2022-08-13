8d3255f503d268689fa517af8db667c393e4310c
Jake <jake@hwll.me>
2022-08-13 16:48:45 +1000

    feat: implement multi-stage smaller docker build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const unstableIncludeFiles = ['.bin', 'bcrypt', '@prisma']
+
 module.exports = {
+  output: 'standalone',
   reactStrictMode: true,
+  unstable_includeFiles: unstableIncludeFiles.map(
+    (d) => `node_modules/${d}/**/*`
+  ),
 }
